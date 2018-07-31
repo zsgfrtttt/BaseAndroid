f79e8f51901bb600fd3912e6c6fb9e6cff3b8789
caishuzhan <1058079995@qq.com>
2018-07-31 21:41:38 +0800

    nestscroll

---
+++ app/src/main/java/com/hydbest/baseandroid/activity/cus_viewgroup/NestScrollActivity.java
@@ -0,0 +1,15 @@
+package com.hydbest.baseandroid.activity.cus_viewgroup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hydbest.baseandroid.R;
+
+public class NestScrollActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nest_scroll);
+    }
+}
---
+++ app/src/main/java/com/hydbest/baseandroid/view/viewgroup/NestScrollLayout.java
@@ -0,0 +1,58 @@
+package com.hydbest.baseandroid.view.viewgroup;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class NestScrollLayout extends LinearLayout implements NestedScrollingParent{
+
+    private int mTopViewHeight;
+
+    public NestScrollLayout(Context context) {
+        this(context,null);
+    }
+
+    public NestScrollLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public NestScrollLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        boolean hiddenTop = dy > 0 && getScrollY() < mTopViewHeight;
+        boolean showTop = dy < 0 && getScrollY() > 0 && !ViewCompat.canScrollVertically(target, -1);
+
+        if (hiddenTop || showTop)
+        {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+    }
+
+    /**
+     * onNestedFling你可以捕获对内部View的fling事件，如果return true则表示拦截掉内部View的事件。
+     * @param target
+     * @param velocityX
+     * @param velocityY
+     * @return
+     */
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        if (getScrollY() >= mTopViewHeight)
+            return false;
+        //fling((int) velocityY);
+        return true;
+    }
+}
