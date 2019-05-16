2541d6f3e52802d5cf395c915a90f2fd67769c1c
zsgfrtttt <1058079995@qq.com>
2019-05-16 17:54:41 +0800

    .

--- app/src/main/java/com/hydbest/baseandroid/activity/HomeActivity.java
+++ app/src/main/java/com/hydbest/baseandroid/activity/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.hydbest.baseandroid.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.hydbest.baseandroid.R;
 import com.hydbest.baseandroid.adapter.ExpandableItemAdapter;
@@ -41,9 +44,24 @@ public class HomeActivity extends AppCompatActivity {
         manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
             @Override
             public int getSpanSize(int position) {
+                if (adapter.getItemViewType(position) == BaseQuickAdapter.LOADING_VIEW){
+                    return manager.getSpanCount();
+                }
                 return adapter.getItemViewType(position) == ExpandableItemAdapter.TYPE_LEVEL_0 ? manager.getSpanCount() : 1;
             }
         });
+        adapter.setEnableLoadMore(true);
+        adapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.loadMoreEnd();
+                    }
+                },3000);
+            }
+        },rv);
 
         rv.setAdapter(adapter);
         rv.setLayoutManager(manager);
