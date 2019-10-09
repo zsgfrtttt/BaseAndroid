5526be413d83817ebfef1ecf34bed1da74bf8087
zsgfrtttt <1058079995@qq.com>
2019-10-09 18:30:25 +0800

    .shadowView

---
+++ app/src/main/java/com/hydbest/baseandroid/activity/cus_view/shadow/sample/ImageAdapter.kt
@@ -0,0 +1,79 @@
+package com.hydbest.baseandroid.activity.cus_view.shadow.sample;
+
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.ViewTarget
+import com.bumptech.glide.request.transition.Transition
+import com.hydbest.baseandroid.R
+import com.hydbest.baseandroid.activity.cus_view.shadow.sample.data.DataSource
+import com.hydbest.baseandroid.activity.cus_view.shadow.sample.data.Item
+import io.github.armcha.coloredshadow.ShadowImageView
+
+/**
+ * let: it访问，返回最后一行
+ * with: this访问 ，返回最后一行
+ * run : this访问，返回最后一行
+ * apply:this访问，返回自身
+ * also :it访问，返回自身
+ */
+
+class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+    private val items by lazy {
+        DataSource.items
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_shadow, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val shadowView by lazy { itemView.findViewById<ShadowImageView>(R.id.shadowView) }
+        private val text by lazy { itemView.findViewById<TextView>(R.id.text) }
+
+        fun bind(item: Item) {
+            text.text = item.name
+            shadowView.radiusOffset = 0.7f
+            //shadowView.shadowColor = Color.GRAY
+            Glide.with(itemView.context)
+                    .load(item.imageUrl)
+                    .apply(RequestOptions().placeholder(R.drawable.place_holder).error(R.drawable.place_holder))
+                    //.transform(CircleCrop())
+                    .into(object : ViewTarget<ImageView, Drawable>(shadowView) {
+                        override fun onLoadStarted(placeholder: Drawable?) {
+                            super.onLoadStarted(placeholder)
+                            shadowView.setImageDrawable(placeholder, withShadow = false)
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            super.onLoadCleared(placeholder)
+                            shadowView.setImageDrawable(placeholder, withShadow = false)
+                        }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) {
+                            super.onLoadFailed(errorDrawable)
+                            shadowView.setImageDrawable(errorDrawable, withShadow = false)
+                        }
+
+                        override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                            shadowView.setImageDrawable(resource)
+                        }
+                    })
+        }
+    }
+}
---
+++ app/src/main/java/com/hydbest/baseandroid/activity/cus_view/shadow/sample/StaticImageActivity.kt
@@ -0,0 +1,38 @@
+package com.hydbest.baseandroid.activity.cus_view.shadow.sample;
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.hydbest.baseandroid.R
+import io.github.armcha.coloredshadow.ShadowImageView
+
+
+class StaticImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_static_image)
+        val image = findViewById<ShadowImageView>(R.id.shadowImage).apply {
+            radiusOffset = 0.4f
+            setImageResource(R.drawable.android)
+        }
+
+        findViewById<Button>(R.id.red).setOnClickListener {
+            image.shadowColor = ContextCompat.getColor(this@StaticImageActivity, R.color.red)
+            image.setImageResource(R.drawable.android)
+        }
+        findViewById<Button>(R.id.blue).setOnClickListener {
+            image.shadowColor = ContextCompat.getColor(this@StaticImageActivity, R.color.blue)
+            image.setImageResource(R.drawable.android)
+        }
+        findViewById<Button>(R.id.green).setOnClickListener {
+            image.shadowColor = ContextCompat.getColor(this@StaticImageActivity, R.color.green)
+            image.setImageResource(R.drawable.android)
+        }
+        findViewById<Button>(R.id.gray).setOnClickListener {
+            image.shadowColor = ContextCompat.getColor(this@StaticImageActivity, R.color.gray)
+            image.setImageResource(R.drawable.android)
+        }
+    }
+}
